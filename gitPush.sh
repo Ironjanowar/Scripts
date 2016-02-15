9a4e02ba46db91ec9a85c128e3266433dba74c1d
ironjanowar <ironjanowar@gmail.com>
2016-02-15 18:11:01 +0100

    Minor fixes

--- gitPush.sh
+++ gitPush.sh
@@ -10,13 +10,13 @@ echo "Adding all modified files from $PWD"
 git add --all
 
 # COMMIT
-echo "Type the commit message"
+echo "Type the commit message: "
 read commitMSG
 
 git commit -m "$commitMSG"
 
 # PUSH
-echo "PUSH to master? [y/n]"
+echo "PUSH to master? [y/n]: "
 read push
 if [ $push == "y" ]; then
     git push origin master
