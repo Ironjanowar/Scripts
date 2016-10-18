26f180b6bd267c6a6f6e66fdec9e5f7eee24e077
Ironjanowar <ironjanowar@gmail.com>
2016-10-18 22:47:06 +0200

    Improved remove_spaces.py

--- remove_spaces.py
+++ remove_spaces.py
@@ -2,13 +2,16 @@ import sys
 import os
 import os.path as path
 
+letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz.áéíóúÁÉÍÓÚ1234567890"
+
 for File in sys.argv:
     if path.isfile(File) and not path.basename(__file__) == File:
         new_name = ""
         for letter in File:
-            if letter == " ":
-                new_name += "_"
-            else:
+            if letter in letters:
                 new_name += letter
+            elif letter == " ":
+                new_name += "_"
+
         os.rename(File, new_name)
         print("File \"" + File + "\" renamed to \"" + new_name + "\"")
