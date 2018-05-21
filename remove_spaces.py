0030a0fc7fb770aa3e012637c6bcbc93af914ee8
Ironjanowar <ironjanowar@gmail.com>
2018-05-21 19:42:40 +0200

    Minor fix

--- remove_spaces.py
+++ remove_spaces.py
@@ -8,4 +8,4 @@ for file_name in sys.argv:
         new_file_name = file_name.replace(' ', '_')
         os.rename(file_name, new_file_name)
         print("File \"" + file_name + "\" renamed to \"" + new_file_name + "\"")
-    print("Renaming finished")
+print("Renaming finished")
